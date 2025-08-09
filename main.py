ab19f050b6fac3034da9eea16f26bd1aac01503d
MannasehM <codingMann007@gmail.com>
2025-08-09 10:30:43 -0400

    Implemented basic camera window

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+import mediapipe
+import pyautogui
+
+camera = cv2.VideoCapture(0)
+while True: 
+    _,image = camera.read()
+    cv2.imshow("Eye controlled mouse", image)
+    key = cv2.waitKey(100)
+    if key == 27: 
+        break
+camera.release()
+cv2.destroyAllWindows()
+
